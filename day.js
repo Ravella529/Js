4266583f77ba559f53cd3c18d260e7bfa5497a15
Naveen <nravella529@gmail.com>
2020-09-16 05:53:33 -0400

    adding get day program

---
+++ day.js
@@ -0,0 +1,19 @@
+
+date = new Date();
+day = date.getDay()
+
+switch(day){
+case 0: console.log("sunday");
+break;
+case 1: console.log("Monday");
+break;
+case 2: console.log("Tuesday");
+break;
+case 3: console.log("Wednesday");
+break;
+case 4: console.log("Thursday");
+break;
+case 5: console.log("Friday");
+break;
+case 6: console.log("Saturday");
+break;}
